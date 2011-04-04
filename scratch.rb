f0058f764df8a6cb06f3c4b47687a873ad43ed5c
cclamb <cclamb@cs.unm.edu>
2011-04-04 07:42:40 -0600

    work in scratch on new syntax

--- scratch.rb
+++ scratch.rb
@@ -1,26 +1,58 @@
-
-$h={:c1=>lambda{true}, :c2=>lambda{false}}
-
-class AndEvaluator
-  def call(h)
-    f = false
-    h.each_value do |v|
-      f = v.call
-      puts f
-      break if f == false
-    end
-    f
-  end
+#
+# Activities must be able to support:
+# is_activity a1
+# is_activity a1, a2
+# is_activity a1, a2, preds
+# etc.
+#
+def is_activity(*args)
+  arguments = parse(args)
+  arguments
 end
 
-class OrEvaluator
-  def call(h)
-    f = false
-    h.each_value do |v|
-      f = v.call
-      puts f
-      break if f == true
+def constrain_by(*args)
+  constraints = Constraints.new
+  args.each do |e|
+    if e.kind_of? Proc
+      constraints << e
+    else
+      raise ArgumentError.new("Incorrect type to constrain_by: #{e.to_s}")
     end
-    f
   end
+  constraints
+end
+
+def obligate_with(*args)
+  obligations = Obligations.new
+  args.each do |e|
+    if e.kind_of? Proc
+      obligations << e
+    else
+      raise ArgumentError.new("Incorrect type to constrain_by: #{e.to_s}")
+    end
+  end
+  obligations
+end
+
+# We need these to differentiate collections of obligations & constraints?
+class Obligations < Array; end
+class Constraints < Array; end
+
+# Helper functions to parse the arg lists
+# parse(*a): Array -> Hash
+# TODO: Really should remove dups in returned lists
+def parse(*a)
+  h = { :obligations => [], :constraints => [], :restricted_activities => []}
+  a.each do |e|
+    if e.kind_of? Obligations
+      h[:obligations] = h[:obligations] + e
+    elsif e.kind_of? Constraints
+      h[:constraints] = h[:constraints] + e
+    elsif e.kind_of? Proc
+      h[:restricted_activities] << e
+    else
+      raise ArgumentError.new("Incorrect type to parse: #{e.to_s}")
+    end
+  end
+  h
 end
